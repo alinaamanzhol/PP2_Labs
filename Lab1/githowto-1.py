fb20b3dc04bf10f26acab11bdd9649fc451fae0b
alinaamanzhol <amanzh17ol@gmail.com>
2025-01-16 14:11:16 +0500

    githowto-1

---
+++ Lab1/githowto-1.py
@@ -0,0 +1,5 @@
+git init
+git config --global user.name "alinaamanzhol"
+git config --global user.email "amanzh17ol@gmail.com"
+git config --global core.autocrlf true
+git config --global core.safecrlf warn
